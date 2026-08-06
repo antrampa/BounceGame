66078db095034afd587ad53233006dc61c7dfcea
Antonis Zalonis <antonis.zalonis@gmail.com>
2026-08-06 22:51:57 +0300

    ball boundaries

--- Source/Ball.cpp
+++ Source/Ball.cpp
@@ -8,7 +8,7 @@ Ball::Ball() :
     shape_.setOrigin(shape_.getGeometricCenter());
     shape_.setPosition({600 * 0.50f, 800 * 0.25f});
 
-    direction_ = {0, -1};
+    direction_ = {1, 0};
 }
 
 void Ball::Draw(sf::RenderWindow& window) const 
@@ -18,5 +18,21 @@ void Ball::Draw(sf::RenderWindow& window) const
 
 void Ball::Move(float deltaTime)
 {
+    sf::Vector2 lastPosition = shape_.getPosition();
+
     shape_.move(direction_ * speed_ * deltaTime);
+
+    if(shape_.getPosition().x < shape_.getRadius() ||
+        shape_.getPosition().x >= 600 - shape_.getRadius()
+    )
+    {
+        shape_.setPosition(lastPosition);
+        direction_.x *= -1;
+    }
+
+    if(shape_.getPosition().y < shape_.getRadius()) 
+    {
+        shape_.setPosition(lastPosition);
+        direction_.y *= -1;
+    }
 }
